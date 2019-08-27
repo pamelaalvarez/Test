c71a97294a60fd40b254a88f212d319715608dbe
Pamela Alvarez <pamela.alvarez@jalasoft.com>
2019-08-27 11:33:10 -0400

    Changing to absolute path to fix error

--- ConsoleApp1/StageProcess.bat
+++ ConsoleApp1/StageProcess.bat
@@ -1,5 +1,5 @@
 setlocal enableextensions
 set name=%date:~-4,4%%date:~-10,2%%date:~7,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
 mkdir c:\\StagingArea\%name%
-copy "bin\Debug\netcoreapp2.1\ConsoleApp1.dll" "c:\\StagingArea\%name%"
+copy "C:\SourceControl\Test\Test\ConsoleApp1\bin\Debug\netcoreapp2.1\ConsoleApp1.dll" "c:\\StagingArea\%name%"
 
