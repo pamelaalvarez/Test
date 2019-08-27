376af68d9755fddc99ba5e4000453e70fbbd599f
Pamela Alvarez <pamela.alvarez@jalasoft.com>
2019-08-27 13:31:55 -0400

    Adding text line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is a test!!");
+            Console.WriteLine("This is a test2!!");
 
         }
     }
